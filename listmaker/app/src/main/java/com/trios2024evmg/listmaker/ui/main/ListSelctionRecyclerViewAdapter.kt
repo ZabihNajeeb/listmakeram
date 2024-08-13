96bbce606a65343fe17bf155cb63807963c9d1b7
ZabihNajeeb <zabih_najeebi@yahoo.com>
2024-08-13 09:02:45 -0400

    fabButton

--- listmaker/app/src/main/java/com/trios2024evmg/listmaker/ui/main/ListSelctionRecyclerViewAdapter.kt
+++ listmaker/app/src/main/java/com/trios2024evmg/listmaker/ui/main/ListSelctionRecyclerViewAdapter.kt
@@ -1,15 +1,18 @@
 package com.trios2024evmg.listmaker.ui.main
-
+import  android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-
+import com.trios2024evmg.listmaker.databinding.ListSelectionViewHolderBinding
 class ListSelectionRecyclerViewAdapter :
     RecyclerView.Adapter<ListSelectionViewHolder>(){
 
         val listTitles = arrayOf("Shopping List", "Chores", "Android Tutorials")
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListSelectionViewHolder {
-        TODO("Not yet implemented")
+        val binding =
+            ListSelectionViewHolderBinding.inflate(LayoutInflater.from(parent.context),
+                parent, false)
+        return ListSelectionViewHolder(binding)
     }
 
     override fun getItemCount(): Int {
@@ -17,6 +20,9 @@ class ListSelectionRecyclerViewAdapter :
     }
 
     override fun onBindViewHolder(holder: ListSelectionViewHolder, position: Int) {
-        TODO("Not yet implemented")
+
+            holder.binding.itemNumber.text = (position + 1).toString()
+            holder.binding.itemString.text = listTitles[position]
+        
     }
 }
--- listmaker/app/src/main/java/com/trios2024evmg/listmaker/ui/main/ListSelectionViewHolder.kt
+++ listmaker/app/src/main/java/com/trios2024evmg/listmaker/ui/main/ListSelectionViewHolder.kt
@@ -1,8 +1,9 @@
 package com.trios2024evmg.listmaker.ui.main
 
 import androidx.recyclerview.widget.RecyclerView
+import com.trios2024evmg.listmaker.databinding.ListSelectionViewHolderBinding
 
-class ListSelectionViewHolder(val binding:ListSelectionViewHolderBinding) :
+class ListSelectionViewHolder(val binding: ListSelectionViewHolderBinding) :
     RecyclerView.ViewHolder(binding.root){
 
     }
